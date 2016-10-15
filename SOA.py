4861a92d269df8c4f1b9daa2a3e522a5e69002af
rafald <rdzbek@gmail.com>
2016-10-15 10:38:39 +0200

    Update SOA.py

--- SOA.py
+++ SOA.py
@@ -8,7 +8,7 @@ for i in xrange(len(first_names)):
     print "Name: %s %s" % (first_names[i], last_names[i])
     print "Height in CM: %s" % heights_in_cm[i]
 
-Using zip:
+# Using zip:
 for first_name, last_name, height in zip(first_names, last_names, heights_in_cm):
     print "Name: %s %s" % (first_name, last_name)
     print "Height in CM: %s" % height_in_cm
