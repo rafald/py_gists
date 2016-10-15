c3a726fcc892ab813f7b32ea30f3830b503d1f0a
Rafal <rdzbek@gmail.com>
2016-10-15 10:45:48 +0200

    make it running on python3 - actually now works for both: pre 3 and post 3

--- SOA.py
+++ SOA.py
@@ -4,12 +4,12 @@ first_names = ['Joe',  'Bob',  'Frank',  'Hans'    ]
 last_names = ['Smith','Seger','Sinatra','Schultze']
 heights_in_cm = [169,     158,    201,      199      ]
 
-for i in xrange(len(first_names)):
-    print "Name: %s %s" % (first_names[i], last_names[i])
-    print "Height in CM: %s" % heights_in_cm[i]
+for i in range(len(first_names)):
+    print ("Name: %s %s" % (first_names[i], last_names[i]) )
+    print ("Height in CM: %s" % heights_in_cm[i])
 
 # Using zip:
 for first_name, last_name, height in zip(first_names, last_names, heights_in_cm):
-    print "Name: %s %s" % (first_name, last_name)
-    print "Height in CM: %s" % height
+    print ("Name: %s %s" % (first_name, last_name))
+    print ("Height in CM: %s" % height)
     
