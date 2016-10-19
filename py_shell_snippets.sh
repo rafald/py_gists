7d4cbceddf605a8e712b8902d3f549e334b7d48b
rafald <rdzbek@gmail.com>
2016-10-19 11:24:59 +0200

    Update and rename py_environment.sh to py_shell_snippets.sh

--- py_shell_snippets.sh
+++ py_shell_snippets.sh
@@ -1,8 +1,13 @@
 
-python3
--m module-name
+python3 -m module-name
               Searches sys.path for the named module and runs the corresponding .py file as a script.
+              
 python3 -m pip list
+             list installed modules
+             
+alias venv3='python3 -m venv '
+
+
 
 
 
