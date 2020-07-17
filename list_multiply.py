688886616e474dc2b854a80d9b2cbae68e6cf3f6
rafald <rdzbek@gmail.com>
2020-07-17 21:05:46 +0200

    Update list_multiply.py
    fix issue 'Invalid syntax FLK-E999'

--- list_multiply.py
+++ list_multiply.py
@@ -3,7 +3,7 @@ myList = [[1] * 4] * 3
 #lst1 = [1]*4
 #myList = [lst1]*3
 myList[1][1] = 7
-print myList
+print (myList)
 
 #When you write [x]*3 you get, essentially, the list [x, x, x]. That is, a list with 3 references to x. When you then change x all three references are changed.
 #To fix it, you need to make sure that you create a new list at each position. One way to do it is
