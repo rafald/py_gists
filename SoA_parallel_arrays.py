5af19776320cd0592b3b3255bc5d12ea2305917d
rafald <rdzbek@gmail.com>
2020-07-17 20:46:19 +0200

    Update SoA_parallel_arrays.py
    fix issue detected by deepsource.io

--- SoA_parallel_arrays.py
+++ SoA_parallel_arrays.py
@@ -4,8 +4,8 @@ first_names = ['Joe',  'Bob',  'Frank',  'Hans'    ]
 last_names = ['Smith','Seger','Sinatra','Schultze']
 heights_in_cm = [169,     158,    201,      199      ]
 
-for i in range(len(first_names)):
-    print ("Name: %s %s" % (first_names[i], last_names[i]) )
+for i,first_name in enumerate(first_names):
+    print ("Name: %s %s" % (first_name, last_names[i]) )
     print ("Height in CM: %s" % heights_in_cm[i])
 
 # Using zip:
